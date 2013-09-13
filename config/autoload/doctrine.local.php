beaee08fd30a258c3bf9af1eabdbc24836b37557
irving <irving@irving-HP-Pavilion-g4-Notebook-PC>
2013-09-13 14:51:50 -0300

    Configuração do Doctrine 2

---
+++ config/autoload/doctrine.local.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+  'doctrine' => array(
+    'connection' => array(
+      'orm_default' => array(
+        'driverClass' =>'Doctrine\DBAL\Driver\PDOMySql\Driver',
+        'params' => array(
+          'host'     => '10.0.0.249',
+          'port'     => '3306',
+          'user'     => 'iedcmcmv',
+          'password' => '@$iedcmcmvq1w2e3r4#%',
+          'dbname'   => 'iedcmcmv',
+)))));
