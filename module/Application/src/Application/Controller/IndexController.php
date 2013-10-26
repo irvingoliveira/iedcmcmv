b0a755cbd0318f734a5fea01a45b3056243203c5
irving <irving@irving-HP-Pavilion-g4-Notebook-PC>
2013-10-26 13:35:59 -0200

    Adicionados termos de licença aos arquivos fonte

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -1,10 +1,27 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
+ * IEDCMCMV (http://mcmv.duquedecaxias.rj.gov.br)
  *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @link      https://github.com/irvingoliveira/iedcmcmv.git para o código-fonte
+ * @copyright Copyright (c) 2013 Irving Fernando de Medeiros Oliveira, Guilherme Sousa dos Santos,
+ * Eduardo Agusto Costa Bastos, Secretaria Municipal de Controle Interno, Ciência, Tecnologia e Sistemas de Duque de Caxias - RJ 
+ * @license   http://www.gnu.org/licenses/gpl.txt GNU General Public Licence Version 3
+ * 
+ * Este arquivo é parte do programa IEDCMCMV
+ *
+ * IEDCMCMV é um software livre; você pode redistribuí-lo e/ou 
+ * modificá-lo dentro dos termos da Licença Pública Geral GNU como 
+ * publicada pela Fundação do Software Livre (FSF); na versão 3 da 
+ * Licença.
+ *
+ * Este programa é distribuído na esperança de que possa ser  útil, 
+ * mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer
+ * MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
+ * Licença Pública Geral GNU para maiores detalhes.
+ * 
+ * Você deve ter recebido uma cópia da Licença Pública Geral GNU
+ * junto com este programa, se não, escreva para a Fundação do Software
+ * Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 namespace Application\Controller;
